db9214374cffdd953bf74f876d8b1e17228de8f1
Germán Moreno <grmmrno@gmail.com>
2020-09-19 11:52:51 -0400

    Proyecto final

--- js/app.js
+++ js/app.js
@@ -22,35 +22,49 @@ const datosBusqueda = {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  mostrarAutos();
+  mostrarAutos(autos);
 
   llenarSelect();
 });
 
-marca.addEventListener("change", (e) => (datosBusqueda.marca = e.target.value));
-yearOptions.addEventListener(
-  "change",
-  (e) => (datosBusqueda.year = e.target.value)
-);
-minimo.addEventListener(
-  "change",
-  (e) => (datosBusqueda.minimo = e.target.value)
-);
-maximo.addEventListener(
-  "change",
-  (e) => (datosBusqueda.maximo = e.target.value)
-);
-puertas.addEventListener(
-  "change",
-  (e) => (datosBusqueda.puertas = e.target.value)
-);
-transmision.addEventListener(
-  "change",
-  (e) => (datosBusqueda.transmision = e.target.value)
-);
-color.addEventListener("change", (e) => (datosBusqueda.color = e.target.value));
+marca.addEventListener("change", (e) => {
+  datosBusqueda.marca = e.target.value;
+  filtrarAuto();
+});
 
-function mostrarAutos() {
+yearOptions.addEventListener("change", (e) => {
+  datosBusqueda.year = parseInt(e.target.value);
+  filtrarAuto();
+});
+
+minimo.addEventListener("change", (e) => {
+  datosBusqueda.minimo = e.target.value;
+  filtrarAuto();
+});
+
+maximo.addEventListener("change", (e) => {
+  datosBusqueda.maximo = e.target.value;
+  filtrarAuto();
+});
+
+puertas.addEventListener("change", (e) => {
+  datosBusqueda.puertas = e.target.value;
+  filtrarAuto();
+});
+
+transmision.addEventListener("change", (e) => {
+  datosBusqueda.transmision = e.target.value;
+  filtrarAuto();
+});
+
+color.addEventListener("change", (e) => {
+  datosBusqueda.color = e.target.value;
+  filtrarAuto();
+});
+
+function mostrarAutos(autos) {
+  limpiarHTML();
+
   autos.forEach((auto) => {
     const autoHTML = document.createElement("p");
     const { marca, modelo, year, puertas, transmision, precio, color } = auto;
@@ -61,6 +75,12 @@ function mostrarAutos() {
 
     resultado.appendChild(autoHTML);
   });
+}
+
+function limpiarHTML() {
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
 }
 
 function llenarSelect() {
@@ -74,4 +94,98 @@ function llenarSelect() {
 
     yearOptions.appendChild(yearHTML);
   }
+}
+
+function filtrarAuto() {
+  const resultado = autos
+    .filter(filtrarMarca)
+    .filter(filtrarAño)
+    .filter(filtrarMinimo)
+    .filter(filtrarMaximo)
+    .filter(filtrarPuertas)
+    .filter(filtrarTransmision)
+    .filter(filtrarColor);
+
+  if (resultado.length) {
+    mostrarAutos(resultado);
+  } else {
+    noResultado();
+  }
+}
+
+function noResultado() {
+  limpiarHTML();
+
+  const noResultado = document.createElement("div");
+  noResultado.classList.add("alerta", "error");
+  noResultado.textContent = "No hay resultados";
+  resultado.appendChild(noResultado);
+}
+
+function filtrarMarca(auto) {
+  const { marca } = datosBusqueda;
+  if (marca) {
+    return auto.marca === marca;
+  }
+
+  return auto;
+}
+
+function filtrarAño(auto) {
+  const { year } = datosBusqueda;
+  if (year) {
+    return auto.year === year;
+  }
+
+  return auto;
+}
+
+function filtrarMinimo(auto) {
+  const { minimo } = datosBusqueda;
+  console.log(minimo);
+  if (minimo) {
+    return auto.precio >= minimo;
+  }
+
+  return auto;
+}
+
+function filtrarMaximo(auto) {
+  const { maximo } = datosBusqueda;
+
+  if (maximo) {
+    return auto.precio <= maximo;
+  }
+
+  return auto;
+}
+
+function filtrarPuertas(auto) {
+  const { puertas } = datosBusqueda;
+
+  if (puertas) {
+    return auto.puertas == puertas;
+  }
+
+  return auto;
+}
+
+function filtrarTransmision(auto) {
+  const { transmision } = datosBusqueda;
+
+  if (transmision) {
+    return auto.transmision === transmision;
+  }
+
+  return auto;
+}
+
+function filtrarColor(auto) {
+  const { color } = datosBusqueda;
+
+  if (color) {
+    return auto.color === color;
+  }
+
+  return auto;
 }
