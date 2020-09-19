907858205b32dbb968eec9681148aa2edd3c4c78
Germán Moreno <grmmrno@gmail.com>
2020-09-19 11:04:30 -0400

    Leyendo los valores de las selecciones

--- js/app.js
+++ js/app.js
@@ -1,14 +1,54 @@
 const resultado = document.querySelector("#resultado");
+
+const marca = document.querySelector("#marca");
 const yearOptions = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+const puertas = document.querySelector("#puertas");
 
 const max = new Date().getFullYear();
 const min = max - 10;
+
+const datosBusqueda = {
+  marca: "",
+  year: "",
+  puertas: "",
+  transmision: "",
+  minimo: "",
+  maximo: "",
+  color: "",
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   mostrarAutos();
 
   llenarSelect();
 });
+
+marca.addEventListener("change", (e) => (datosBusqueda.marca = e.target.value));
+yearOptions.addEventListener(
+  "change",
+  (e) => (datosBusqueda.year = e.target.value)
+);
+minimo.addEventListener(
+  "change",
+  (e) => (datosBusqueda.minimo = e.target.value)
+);
+maximo.addEventListener(
+  "change",
+  (e) => (datosBusqueda.maximo = e.target.value)
+);
+puertas.addEventListener(
+  "change",
+  (e) => (datosBusqueda.puertas = e.target.value)
+);
+transmision.addEventListener(
+  "change",
+  (e) => (datosBusqueda.transmision = e.target.value)
+);
+color.addEventListener("change", (e) => (datosBusqueda.color = e.target.value));
 
 function mostrarAutos() {
   autos.forEach((auto) => {
